3a26bf6c47e18719385ba0afdf59154c86f61cc2
Roge Rodrigues <roge17@gmail.com>
2020-03-28 23:35:01 -0300

    Alteração geral do layout e páginas

--- src/Images/Projetos/script.js
+++ src/Images/Projetos/script.js
@@ -14,35 +14,6 @@ function currentSlide(n) {
 function showSlides(n) {
   var i;
   var slides = document.getElementsByClassName("mySlides");
-  var dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-      slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-      dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
-
-var slideIndex = 1;
-showSlides(slideIndex);
-
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
-
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  var i;
-  var slides = document.getElementsByClassName("mySlides");
   var dots = document.getElementsByClassName("demo");
   var captionText = document.getElementById("caption");
   if (n > slides.length) {slideIndex = 1}
@@ -57,3 +28,52 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+const modal = document.querySelector('.modal')
+const menu = document.querySelector('.open-modal')
+
+    menu.addEventListener("click", function () {
+    modal.classList.add('active')
+})
+
+    document.querySelector('.modal').addEventListener("click", function () {
+    modal.classList.remove('active')
+    })
+
+    document.onkeydown = function(e) {
+      switch (e.keyCode) {
+          case 37:
+            document.querySelector('.prev').click()
+              break;
+          case 39:
+              document.querySelector('.next').click()
+              break;
+      }
+  }
+
+  var elem = document.querySelector(".container")
+
+  /* View in fullscreen */
+  function openFullscreen() {
+    if (elem.requestFullscreen) {
+      elem.requestFullscreen();
+    } else if (elem.mozRequestFullScreen) { /* Firefox */
+      elem.mozRequestFullScreen();
+    } else if (elem.webkitRequestFullscreen) { /* Chrome, Safari and Opera */
+      elem.webkitRequestFullscreen();
+    } else if (elem.msRequestFullscreen) { /* IE/Edge */
+      elem.msRequestFullscreen();
+    }
+  }
+
+  function closeFullscreen() {
+    if (document.exitFullscreen) {
+      document.exitFullscreen();
+    } else if (document.mozCancelFullScreen) { /* Firefox */
+      document.mozCancelFullScreen();
+    } else if (document.webkitExitFullscreen) { /* Chrome, Safari and Opera */
+      document.webkitExitFullscreen();
+    } else if (document.msExitFullscreen) { /* IE/Edge */
+      document.msExitFullscreen();
+    }
+  }
--- src/Pages/Companies/scripts.js
+++ src/Pages/Companies/scripts.js
@@ -20,14 +20,22 @@ for(let card of cards){
     card.addEventListener("click", function() {
         var folder = card.getAttribute('id')
         overlay.classList.add('active')
-        document.querySelector('.modal-house iframe').src = `/src/Images/Projetos/empresarial${folder}/index.html`
+        document.querySelector('.iframe').href = `/src/Images/Projetos/casa${folder}/index.html`
 
         
     })
 }
 
-    document.querySelector('.close-modal').addEventListener("click", function () {
+    document.querySelector('.close-modal','.modal-overlay').addEventListener("click", function () {
     overlay.classList.remove('active')
-    document.querySelector('.modal-house iframe').src = ''
+    document.querySelector('iframe').src = ''
     
     })
+
+    // Handle ESC key (key code 27)
+document.addEventListener('keyup', function(e) {
+    if (e.keyCode == 27) {
+        overlay.classList.remove('active')
+        document.querySelector('iframe').src = ''
+    }
+});
