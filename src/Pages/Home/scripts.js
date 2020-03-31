5b31a0ae7f53b1ab2432967e9a65361697dc9fcb
Roge Rodrigues <roge17@gmail.com>
2020-03-31 19:23:38 -0300

    Inserção da animação na home page

--- src/Pages/Home/scripts.js
+++ src/Pages/Home/scripts.js
@@ -1,7 +1,7 @@
 const modal = document.querySelector('.modal')
 const menu = document.querySelector('.open-modal')
 const cards = document.querySelectorAll('.card')
-
+const links = document.querySelectorAll('.pages')
 
     menu.addEventListener("click", function () {
     modal.classList.add('active')
@@ -17,12 +17,21 @@ const cards = document.querySelectorAll('.card')
         const link = card.getAttribute('id')
             document.getElementById(link).style.background = 'white'
             document.getElementById(link).style.color = 'black'
-     
+            document.querySelector('.dalmata').src = `/src/Pages/assets/animation/${link}.png`
     })
     const link = card.getAttribute('id')
     card.addEventListener("mouseout",function(){
         document.getElementById(link).style.background = ''
-            document.getElementById(link).style.color = ''
- 
+        document.getElementById(link).style.color = ''
+        document.querySelector('.dalmata').src = `/src/Pages/assets/animation/house.png`
+    })
+}
+
+for(let link of links){
+    link.addEventListener("mouseover",function(){
+
+        const page= link.getAttribute('id')
+        // const link = card.getAttribute('id')
+            document.querySelector('.dalmata').src = `/src/Pages/assets/animation/${page}.png`
     })
 }
