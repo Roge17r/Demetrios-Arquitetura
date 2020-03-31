82983dc463ca988f8686b999875a116bfba177d4
Roge Rodrigues <roge17@gmail.com>
2020-03-31 15:24:06 -0300

    Adicionado efeito nas opções da página home

--- src/Pages/Home/scripts.js
+++ src/Pages/Home/scripts.js
@@ -1,5 +1,7 @@
 const modal = document.querySelector('.modal')
 const menu = document.querySelector('.open-modal')
+const cards = document.querySelectorAll('.card')
+
 
     menu.addEventListener("click", function () {
     modal.classList.add('active')
@@ -8,3 +10,19 @@ const menu = document.querySelector('.open-modal')
     document.querySelector('.modal').addEventListener("click", function () {
     modal.classList.remove('active')
     })
+   
+    for(let card of cards){
+    card.addEventListener("mouseover",function(){
+        
+        const link = card.getAttribute('id')
+            document.getElementById(link).style.background = 'white'
+            document.getElementById(link).style.color = 'black'
+     
+    })
+    const link = card.getAttribute('id')
+    card.addEventListener("mouseout",function(){
+        document.getElementById(link).style.background = ''
+            document.getElementById(link).style.color = ''
+ 
+    })
+}
